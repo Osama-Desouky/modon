305b2cb46a523955f9a7a04df470453a845940c5
Osama Desoukey <mrsemsim@gmail.com>
2020-09-28 05:43:25 +0300

    Fixed

--- Modon_New_HTML/assets/js/new-script.js
+++ Modon_New_HTML/assets/js/new-script.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-  var productsSlider = new Swiper('.products-slider', {
+$(document).ready(function () {
+  var productsSlider = new Swiper(".products-slider", {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: '.swiper-pagination',
-    }
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
   $("a.fancy").fancybox();
-  $(".slide-toggle").on('click',function(){
-   var toggler= $(this);
-   toggler.addClass("active");
-   var wrapper=$(this).parent().parent().find(".slide-wrapper");
-   wrapper.fadeIn();
-   wrapper.find(".slide-close").on('click',function(){
-    toggler.removeClass("active");
-    wrapper.fadeOut();
-   })
-  })
-})
+  $(".slide-toggle").on("click", function () {
+    var toggler = $(this);
+    toggler.toggleClass("active");
+    var wrapper = $(this).parent().parent().find(".slide-wrapper");
+    wrapper.fadeToggle();
+    wrapper.find(".slide-close").on("click", function () {
+      toggler.removeClass("active");
+      wrapper.fadeOut();
+    });
+  });
+});
